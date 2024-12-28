82d5a4cd7e236d2d9b0228be3e6042dcf4a680b3
Luis Felipe Fernandes Jaciuk <felipejaciuk321@gmail.com>
2024-12-27 23:47:29 -0300

    char

---
+++ curso/src/curso/Caracteres.java
@@ -0,0 +1,20 @@
+package curso;
+
+public class Caracteres {
+
+	//main é um metodo auto executavel em Java
+		public static void main(String[] args) {
+			
+			//char representa uma letra
+			char pessoaFeminino = 'F';
+			
+			if(pessoaFeminino == 'F') {
+				System.out.println("Feminino");
+			} else {
+				System.out.println("Masculino");
+			}
+			
+			
+			
+		}
+}
