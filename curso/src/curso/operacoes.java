e44c5403255dd3b3de1176c5dab9784cf8ee1ef0
Luis Felipe Fernandes Jaciuk <felipejaciuk321@gmail.com>
2024-12-27 23:17:04 -0300

    start

---
+++ curso/src/curso/operacoes.java
@@ -0,0 +1,16 @@
+package curso;
+
+public class operacoes {
+
+	//main é um metodo auto executavel em Java
+	public static void main(String[] args) {
+		
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
+		
+		System.out.println("Media das notas = " + (nota1 + nota2 + nota3 + nota4) / 4);
+		
+	}
+}
