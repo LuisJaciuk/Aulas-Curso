63d2a6851216cd3c4dd245f94d5cc28b6ac104f0
Luis Felipe Fernandes Jaciuk <felipejaciuk321@gmail.com>
2024-12-28 00:03:05 -0300

    Sttrings e concatenações

---
+++ curso/src/curso/strings.java
@@ -0,0 +1,18 @@
+package curso;
+
+public class strings {
+
+	//main é um metodo auto executavel em Java
+			public static void main(String[] args) {
+				
+				//String no Java sempre sera um texto de tamanho qualquer
+				String nome = "Luis Felipe";
+				String cpf = "710.132.401-03";
+				String telefone = "61996192341";
+				String endereço = "r eng ovisio p";
+				
+				System.out.println("Meu nome: " + nome + "." + " Meu Cpf: " + cpf + ".");
+				
+				
+			}
+}
